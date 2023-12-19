7452c2e53a8d055d8401576d82c1fdc93bf26889
Md Parvez Ahamed <devparvez23@gmail.com>
2023-12-19 16:15:27 +0600

    javaScript basic to advance

---
+++ JavaScript/matha.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * *
+ * number : 1,2,3,4,5
+ * Integer: 1,2,3
+ * Float: 5.5,2.5
+ * positive/negative number : 5,-5
+ * Operation : addition+,Subtractions-,multiplication*,Division/,
+ * Percentage
+ * Average
+ * Comparison
+ * <,>,== !==
+ * Remainder,Modulus
+ *  */
